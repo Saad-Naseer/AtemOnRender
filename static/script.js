195aa9910f411ff63797c1afda91b544c868056b
Saad <78992081+Saad-Naseer@users.noreply.github.com>
2024-11-01 12:51:53 +0100

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,4 +1,10 @@
-const socket = io();  // Initialize Socket.IO client
+//const socket = io();  // Initialize Socket.IO client
+const socket = io('https://atem-xzrj.onrender.com/', {
+    transports: ['polling'],
+    cors: {
+        origin: '*',
+    }
+});
 
 document.getElementById('setIpBtn').addEventListener('click', () => {
     const ip = document.getElementById('ipAddress').value;
